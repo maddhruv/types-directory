ca1ec03fc885805f2446c329cc76165e3a6da678
maddhruv <dhruvjainpenny@gmail.com>
2020-01-04 23:40:17 +0530

    add config

--- release.js
+++ release.js
@@ -4,6 +4,10 @@ const { execSync } = require('child_process');
 (async function () {
   const files = await git().status()
   if(files.modified.includes('directory.json')) {
+    await git().addConfig('user.email', 'dhruvjainpenny@gmail.com')
+    
+    await git().addConfig('user.name', 'maddhruv')
+    
     await git().add('./*')
 
     console.log('GIT ADD')
